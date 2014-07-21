528167f522078e332780aa18e6ccc005a1342688
STH <seanthutchison@gmail.com>
2014-07-21 10:10:17 -0500

    Initial Commit - Folder structure established.

---
+++ js/geomath.js
@@ -0,0 +1,8 @@
+/**
+* @author Sean Taylor Hutchison
+* @email seanthutchison@gmail.com
+* @website http://taylorhutchison.com
+* @created 7/21/2014
+* @last_modified 7/21/2014
+*/
+//# sourceMappingURL=geomath.js.map
